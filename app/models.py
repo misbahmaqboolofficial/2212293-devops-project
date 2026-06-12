df8cb4846248f70a34d8667a9e2a9e004be10479
Misbah Maqbool <misbahmaqboolofficial@gmail.com>
2026-06-12 16:03:45 +0500

    fix: rename __init__.py files and fix models and database

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from .database import Base
 class Student(Base):
     __tablename__ = "students"
 
-    id = Column(Integer, primary key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     reg_no = Column(String, unique=True, index=True, nullable=False)
     name = Column(String, nullable=False)
     semester = Column(Integer, nullable=False)
